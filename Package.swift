058216bd0ff4c528fe7a2d8808d0765570269172
Tom OMalley <9475035+tomalley104@users.noreply.github.com>
2025-01-28 10:44:10 -0500

    [release] FB 11.7.0 w/ Catalyst conformance

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "Firebase",
-  platforms: [.iOS(.v11), .macOS(.v10_12), .tvOS(.v12), .watchOS(.v7)],
+  platforms: [.iOS(.v11), .macCatalyst(.v15), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v7)],
   products: [
     .library(
       name: "FirebaseABTesting",
@@ -177,7 +177,7 @@ let package = Package(
         "_FirebaseAuthInterop",
         "_FirebaseCoreExtension",
         "_GTMSessionFetcher",
-        .target(name: "_RecaptchaInterop", condition: .when(platforms: [.iOS]))
+        .target(name: "_RecaptchaInterop", condition: .when(platforms: [.iOS, .macCatalyst]))
       ],
       path: "Sources/FirebaseAuth"
     ),
@@ -216,7 +216,7 @@ let package = Package(
       dependencies: [
         "Firebase",
         "FirebaseAnalyticsTarget",
-        .target(name: "_FirebaseDynamicLinks", condition: .when(platforms: [.iOS]))
+        .target(name: "_FirebaseDynamicLinks", condition: .when(platforms: [.iOS, .macCatalyst]))
       ],
       path: "Sources/FirebaseDynamicLinks"
     ),
@@ -292,7 +292,7 @@ let package = Package(
         "FirebaseAnalyticsTarget",
         "_FirebaseABTesting",
         "_FirebaseCoreExtension",
-        .target(name: "_FirebasePerformance", condition: .when(platforms: [.iOS, .tvOS])),
+        .target(name: "_FirebasePerformance", condition: .when(platforms: [.iOS, .tvOS, .macCatalyst])),
         "_FirebaseRemoteConfig",
         "_FirebaseRemoteConfigInterop",
         "_FirebaseSessions",
@@ -355,11 +355,11 @@ let package = Package(
       dependencies: [
         "Firebase",
         "FirebaseAnalyticsTarget",
-        .target(name: "_AppAuth", condition: .when(platforms: [.iOS])),
+        .target(name: "_AppAuth", condition: .when(platforms: [.iOS, .macCatalyst])),
         "_AppCheckCore",
-        .target(name: "_GTMAppAuth", condition: .when(platforms: [.iOS])),
+        .target(name: "_GTMAppAuth", condition: .when(platforms: [.iOS, .macCatalyst])),
         "_GTMSessionFetcher",
-        .target(name: "_GoogleSignIn", condition: .when(platforms: [.iOS]))
+        .target(name: "_GoogleSignIn", condition: .when(platforms: [.iOS, .macCatalyst]))
       ],
       path: "Sources/GoogleSignIn"
     ),
